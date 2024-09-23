a9a5492406efb66d0ab00e1d17f747b59e90df4d
Vladislav <vladshatl123@gmail.com>
2024-09-23 11:43:38 +0300

    class Money

--- src/main/java/org/example/Dollar.java
+++ src/main/java/org/example/Dollar.java
@@ -2,15 +2,11 @@ package org.example;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Dollar {
-    private int amount ;
+public class Dollar extends Money{
     Dollar (int amount){
         this.amount = amount;
     }
-    public boolean equals(Object object){
-        Dollar dollar = (Dollar)object;
-        return amount == dollar.amount;
-    }
+
     Dollar times(int multiplier){
         return new Dollar(amount * multiplier);
 
