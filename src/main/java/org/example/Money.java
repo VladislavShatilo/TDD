978aeb22ba7801f62056f3af0e261b0c9cfd95e1
Vladislav <vladshatl123@gmail.com>
2024-09-23 11:59:55 +0300

    abstract class Money

--- src/main/java/org/example/Money.java
+++ src/main/java/org/example/Money.java
@@ -1,7 +1,14 @@
 package org.example;
 
-public class Money {
+abstract class Money {
     protected int amount;
+    abstract Money times(int multiplier);
+    static Money dollar(int amount){
+        return new Dollar(amount);
+    }
+    static Money franc(int amount){
+        return new Franc(amount);
+    }
     public boolean equals(Object object){
         Money money = (Money) object;
         return amount == money.amount && getClass().equals(money.getClass());
