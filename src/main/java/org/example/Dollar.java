e8a31f051a589a3acc80df5d0aee649e746d335d
Vladislav <vladshatl123@gmail.com>
2024-09-23 10:53:03 +0300

    equal method

--- src/main/java/org/example/Dollar.java
+++ src/main/java/org/example/Dollar.java
@@ -7,9 +7,12 @@ public class Dollar {
     Dollar (int amount){
         this.amount = amount;
     }
+    public boolean equals(Object object){
+        Dollar dollar = (Dollar)object;
+        return amount == dollar.amount;
+    }
     Dollar times(int multiplier){
         return new Dollar(amount * multiplier);
-
 
     }
     public static void main(String[] args) {
