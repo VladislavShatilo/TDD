7dc3b01a8e55989e2bfdbceedb4d9b1b0d3dd28e
Vladislav <vladshatl123@gmail.com>
2024-09-23 11:50:10 +0300

    equal between Franc and Dollars

--- src/test/java/org/example/TestMultiplication.java
+++ src/test/java/org/example/TestMultiplication.java
@@ -26,6 +26,7 @@ class TestMultiplication {
         assertFalse(new Dollar(5).equals(new Dollar(6)));
         assertTrue(new Franc(5).equals(new Franc(5)));
         assertFalse(new Franc(5).equals(new Franc(6)));
+        assertFalse(new Franc(5).equals(new Dollar(5)));
     }
 
 }
